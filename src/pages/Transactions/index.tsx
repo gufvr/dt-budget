6b2fa31b655f6157e93663050514f78fe0756517
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-07-20 20:04:01 -0300

    making the transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,82 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
+
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>19/07/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$ 59,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>15/07/2023</td>
+            </tr>
+            {/* <tr>
+            <td width="50%">Computador</td>
+            <td>R$ 5.400,00</td>
+            <td>Venda</td>
+            <td>19/07/2023</td>
+          </tr>
+          <tr>
+            <td width="50%">Computador</td>
+            <td>R$ 5.400,00</td>
+            <td>Venda</td>
+            <td>19/07/2023</td>
+          </tr>
+          <tr>
+            <td width="50%">Computador</td>
+            <td>R$ 5.400,00</td>
+            <td>Venda</td>
+            <td>19/07/2023</td>
+          </tr>
+          <tr>
+            <td width="50%">Computador</td>
+            <td>R$ 5.400,00</td>
+            <td>Venda</td>
+            <td>19/07/2023</td>
+          </tr>
+          <tr>
+            <td width="50%">Computador</td>
+            <td>R$ 5.400,00</td>
+            <td>Venda</td>
+            <td>19/07/2023</td>
+          </tr>
+          <tr>
+            <td width="50%">Computador</td>
+            <td>R$ 5.400,00</td>
+            <td>Venda</td>
+            <td>19/07/2023</td>
+          </tr>
+          <tr>
+            <td width="50%">Computador</td>
+            <td>R$ 5.400,00</td>
+            <td>Venda</td>
+            <td>19/07/2023</td>
+          </tr> */}
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
